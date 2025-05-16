8839763d2e8f7429912844f4e51dc0f74df18fe6
KentGab27 <155821672+KentGab27@users.noreply.github.com>
2025-05-16 18:54:05 +0800

    Add files via upload

---
+++ Exercise - Array of Multiples/Exercise - Array of Multiples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Exercise___Array_of_Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 4;
+            int length = 9;
+            int[] result = new int[length];
+            int counter = 0;
+            
+            for (int i = 1; i <= result.Length; i++, counter++)
+            {
+                result[counter] = num * i;
+            }
+
+            foreach (var item in result)
+            {
+                Console.Write($"{item} ");
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
